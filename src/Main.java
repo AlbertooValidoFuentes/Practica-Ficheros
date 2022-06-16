28c90d236300573fab97a2bc78766a14d52fbbdb
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2022-06-16 07:51:13 +0000

    Feature: Agregamos bucle y estructura switch

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,35 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         boolean salir = false;
 
-        System.out.println("1. Inicializar F18. ");
-        System.out.println("2. Alternar estado de los flaps. ");
-        System.out.println("3. Alternar estado del tren de aterrizaje. ");
-        System.out.println("4. Armar sistema de eyeccion. ");
-        System.out.println("5. Eyectar piloto. ");
-        System.out.println("Q. Salir");
+        while (!salir) {
+
+            System.out.println("1. Inicializar F18. ");
+            System.out.println("2. Alternar estado de los flaps. ");
+            System.out.println("3. Alternar estado del tren de aterrizaje. ");
+            System.out.println("4. Armar sistema de eyeccion. ");
+            System.out.println("5. Eyectar piloto. ");
+            System.out.println("Q. Salir");
+
+            System.out.println("Ingrese una opcion: ");
+            String opcion = scan.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "Q":
+                    break;
+            }
+
+        }
+
     }
 
 
