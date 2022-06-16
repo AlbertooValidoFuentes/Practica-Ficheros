ac49e3807e54785756aaabd4c79a38cedceb1108
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2022-06-16 07:43:12 +0000

    Feature: Añadida estructura principal del menú

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        boolean salir = false;
+
+        System.out.println("1. Inicializar F18. ");
+        System.out.println("2. Alternar estado de los flaps. ");
+        System.out.println("3. Alternar estado del tren de aterrizaje. ");
+        System.out.println("4. Armar sistema de eyeccion. ");
+        System.out.println("5. Eyectar piloto. ");
+        System.out.println("Q. Salir");
     }
+
+
 }
